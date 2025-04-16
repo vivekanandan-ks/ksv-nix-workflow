8c466325698b29f70b3b454fdf0d8b876a42810a
vivekanandan-ks <ksvdevksv@gmail.com>
2025-04-16 15:46:23 +0530

    Added python app binary build
    Now u can run the app directly

--- flake.nix
+++ flake.nix
@@ -15,17 +15,39 @@
         #then do the below command before starting the shell(nix develop)
         #nix run github:nix-community/pip2nix -- generate -r requirements.txt
         #packageOverrides = pkgs.callPackage ./python-packages.nix {};
-        #python = pkgs.python313.override { inherit packageOverrides; };
+        #python = pkgs.python3.override { inherit packageOverrides; };
+        pythonEnv = pkgs.python3.withPackages (ps: with ps; [
+          #python packages from pip
+          fastapi
+          uvicorn
+        ]);
+
+        # The Python application
+        myApp = pkgs.stdenv.mkDerivation {
+          name = "my-python-app";
+          src = ./.; # Use the current directory as source
+          
+          # Install phase: copy the Python files to the output
+          installPhase = ''
+            mkdir -p $out/app
+            cp -r app $out/app
+            mkdir -p $out/bin
+            cat > $out/bin/start-app <<EOF
+            #!${pkgs.bash}/bin/bash
+            exec python $out/app/main.py
+            EOF
+            chmod +x $out/bin/start-app
+          '';
+        };
+
+
+
+
+
+        
       in {
         devShells.default = pkgs.mkShell {
-          packages = [
-            (pkgs.python3.withPackages (p: with p ; [
-              #python packages from pip
-              fastapi
-              uvicorn
-              
-              ]))
-          ];
+          packages = pythonEnv;
           shellHook = ''
           echo "You have entered a shell environment created by https://github.com/vivekanandan-ks"
           echo "This shell environment sets up all the dependencies for the python project down to the libraries needed."
@@ -33,6 +55,12 @@
 
           ''
         };
+
+        packages = {
+          python-app = myApp;
+          #ocker = dockerImage; #plannning to build next
+        }
+
       }
     );
 }
